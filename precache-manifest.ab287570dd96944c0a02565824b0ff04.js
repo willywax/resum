15fbfa8b1b3eb2e5b0a1c1d867638c92f39851dc
WillyWax <williamtrans2011@gmail.com>
2019-03-19 08:59:39 +0300

    Updates

---
+++ precache-manifest.ab287570dd96944c0a02565824b0ff04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c4cc25d78c9f8984b74",
+    "url": "/resume/static/css/main.22897322.chunk.css"
+  },
+  {
+    "revision": "7c4cc25d78c9f8984b74",
+    "url": "/resume/static/js/main.f4442433.chunk.js"
+  },
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  },
+  {
+    "revision": "d19503d9bf98999ae8d9",
+    "url": "/resume/static/js/2.37a99ec9.chunk.js"
+  },
+  {
+    "revision": "57fa241c428f442af1ba6b715ee9198f",
+    "url": "/resume/static/media/logo.57fa241c.JPG"
+  },
+  {
+    "revision": "37b2776e367ccd3b5121eeeda9cbf7fa",
+    "url": "/resume/index.html"
+  }
+];
